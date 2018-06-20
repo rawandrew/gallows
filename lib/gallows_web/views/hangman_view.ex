051a204ef0a4f713292ccdb21d937f383cfbd40b
Andrei Crudu <andrei.crudu@yahoo.com>
2018-06-20 16:43:00 +0300

    Make the game show you the game progress.

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -1,3 +1,27 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
+
+  @responses %{
+    :won          => { :success, "You Won!" },
+    :lost         => { :danger, "You Lost!" },
+    :good_guess   => { :success, "Good guess!" },
+    :bad_guess    => { :warning, "Bad guess!" },
+    :already_used => { :info, "You already guesses that" },
+  }
+
+  def game_state(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  defp alert(nil), do: ""
+
+  defp alert({class, message}) do
+    """
+      <div class="alert alert-#{class}">
+        #{message}
+      </div>
+    """
+    |> raw()
+  end
 end
